deceec87e95bbf9ee29dd2fa60abd646e9d7a732
Borodach16 <riuch56@mail.ru>
2021-09-10 17:48:24 +0500

    Немного писанины в app.js

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-const express = require('express')
-const path = require('path');
-const app = express()
-const port = 3000
+import express from 'express';
+import { Client } from 'pg';
+
+const app = express();
+
+const client = new Client({
+  user: 'backoffice',
+  host: 'main.psaa.ru',
+  database: 'db_backoffice',
+  password: 'W30082021',
+  port: '5432',
+});
+client.connect();
 
 app.get('/', (req, res) => {
-//   res.send('Hello Worldsadf asdf asd f!')
-  res.sendFile(path.resolve('./views/index.html'));
-})
+  let query = 'SELECT * FROM tblformtypes';
+  console.log(client.query(query));
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(3000, function() {
+  console.log("смотрим работу через браузер - http://localhost:3000/");
+  let isWin = process.platform === "win32";
+  let hotKeys = isWin? "Ctrl+C": "Ctrl+D"; // Windows / Linux
+  console.log(`остановить сервер - ${hotKeys}`);
+});
