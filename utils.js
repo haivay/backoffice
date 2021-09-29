b9bcf908f69533bc2124bcff023cadbf4485ea80
Borodach16 <riuch56@mail.ru>
2021-09-29 15:09:15 +0500

    Правка запросов.

--- utils.js
+++ utils.js
@@ -12,38 +12,38 @@ const bdParams = {
 export const client = new Client(bdParams);
 
 export async function getForms() {
-  const query = "SELECT * FROM practice.tblformtypes";
+  const query = "SELECT * FROM backoffice.tblformtypes";
   const query_result = await client.query(query);
   return query_result.rows
 }
 
 export function saveForm(formName, formFields) {
-  const query = "INSERT INTO practice.tblformtypes(type_name, document_fields) VALUES ($1, $2)";
+  const query = "INSERT INTO backoffice.tblformtypes(type_name, document_fields) VALUES ($1, $2)";
   client.query(query, [formName, formFields]);
 }
 
 export function deleteForm(id) {
-  const query = "DELETE FROM practice.tblformtypes WHERE id = $1;";
+  const query = "DELETE FROM backoffice.tblformtypes WHERE id = $1;";
   client.query(query, [id]);
 }
 
 export function deleteData(id) {
-  const query = "DELETE FROM practice.tblformrequest WHERE type_id = $1;";
+  const query = "DELETE FROM backoffice.tblformrequest WHERE type_id = $1;";
   client.query(query, [id]);
 }
 
 export function updateForm(id, formName, formFields) {
-  const query = "UPDATE practice.tblformtypes SET type_name = $2, document_fields = $3 WHERE id = $1;";
+  const query = "UPDATE backoffice.tblformtypes SET type_name = $2, document_fields = $3 WHERE id = $1;";
   client.query(query, [id, formName, formFields]);
 }
 
 export function sendData(formId, formData){
-  const query = "INSERT INTO practice.tblformrequest(type_id, request_data, ts) VALUES($1, $2, $3)";
+  const query = "INSERT INTO backoffice.tblformrequest(type_id, request_data, ts) VALUES($1, $2, $3)";
   client.query(query, [formId, formData, 'now']);
 }
 
 export async function getData(typeId){
-  const query = "SELECT request_data, ts FROM practice.tblformrequest WHERE type_id = $1";
+  const query = "SELECT request_data, ts FROM backoffice.tblformrequest WHERE type_id = $1";
   const queryResult = await client.query(query, [typeId]);
   return queryResult.rows
 }
